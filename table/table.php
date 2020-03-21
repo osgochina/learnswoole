db24432936ced8d6ef60703b5f6f7296f0518e1c
clownfish <liuzhiming1@chelun.com>
2020-03-21 22:25:47 +0800

    add table.php

--- table/table.php
+++ table/table.php
@@ -9,6 +9,7 @@ $table->create();
 
 for ($n = 1; $n <= 3; $n++) {
     $process = new Process(function () use ($n,$table) {
+        swoole_set_process_name("child process {$n}");
         echo "child process ".getmypid()." start\n";
         $table->set(getmypid(),['pid'=>getmypid(),'data'=>'process create time:'.date("Y-m-d H:i:s")]);
         sleep(60);
@@ -16,7 +17,7 @@ for ($n = 1; $n <= 3; $n++) {
     $process->start();
     sleep(1);
 }
-
+swoole_set_process_name("master process");
 foreach ($table as $key=>$val)
 {
     echo $key."=>".$val['data']."\n";
