e6be0c11a36adcf7ad69dffcbdfa486f4a713a50
clownfish <liuzhiming1@chelun.com>
2020-03-21 22:06:14 +0800

    add table.php

---
+++ table/table.php
@@ -0,0 +1,24 @@
+<?php
+use Swoole\Process;
+
+$table = new Swoole\Table(1024);
+$table->column('pid', Swoole\Table::TYPE_INT);
+$table->column('data', Swoole\Table::TYPE_STRING, 64);
+$table->create();
+
+
+for ($n = 1; $n <= 3; $n++) {
+    $process = new Process(function () use ($n,$table) {
+        echo "child process ".getmypid()." start\n";
+        $table->set(getmypid(),['pid'=>getmypid(),'data'=>'process create time:'.date("Y-m-d H:i:s")]);
+        sleep(60);
+    });
+    $process->start();
+    sleep(1);
+}
+
+foreach ($table as $key=>$val)
+{
+    echo $key."=>".$val['data']."\n";
+}
+sleep(60);
